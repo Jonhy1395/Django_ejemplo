6cb2581426ddf078ac27aace703d9d3543c99bc3
Jonathan Avila <jonandresavila@hotmail.com>
2022-09-01 19:23:59 -0500

    primer commit con python y django

---
+++ holamundo/holamundo/urls.py
@@ -0,0 +1,8 @@
+from xml.etree.ElementInclude import include
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('miembros/', include('miembros.urls')),
+    path('admin/', admin.site.urls),
+]
